1ab69387cd5eb47e15d0d6c44cd4fa9bbdab95c7
kho <kho@bluefrogrobotics.com>
2023-06-30 15:26:29 +0200

    resize and display

--- app/src/main/java/com/bfr/opencvapp/MainActivity.java
+++ app/src/main/java/com/bfr/opencvapp/MainActivity.java
@@ -230,255 +230,68 @@ public class MainActivity extends CameraActivity implements CameraBridgeViewBase
 
         int x = 0;
         int y = 0;
-//
-//        Mat supResMat = new Mat();
-//        Mat inSuperReso = new Mat();
-////        Imgproc.cvtColor(frame, inSuperReso, Imgproc.COLOR_RGBA2RGB);
-//        Imgproc.cvtColor(frame, inSuperReso, Imgproc.COLOR_RGB2YCrCb);
-//        mSupRes.upsample(inSuperReso, supResMat);
-//
-//        Thread wechat = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                wechatDetector.setScaleFactor(1.0f);
-//                qrCodesCorner = new ArrayList<Mat>();
-//                qrCodesContent = wechatDetector.detectAndDecode(frame, qrCodesCorner);
-//            }
-//        });
-//
-//        wechat.start();
-//
-//
-//        // Super resolution
-////        Mat frame_resize = new Mat();
-////        Imgproc.resize(frame, frame_resize, new Size(224, 224));
-////        Mat frame_resize = frame.clone();
-////        Imgproc.cvtColor(frame_resize, frame_resize, Imgproc.COLOR_RGB2GRAY);
-////        Mat blob = Dnn.blobFromImage(frame_resize, 1/255.0,
-////                new org.opencv.core.Size(224, 224),
-////                new Scalar(0.0, 0.0, 0.0), /*swapRB*/false, /*crop*/false);
-////        superResNet.setInput(blob);
-////        Mat superResMat = superResNet.forward();
-////        Log.w(TAG, "mysize:  " + superResMat.size());
-//
-//
-        /*** Traditional QRCode detection
-         *
-         */
-        Thread tradi = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                QRCodeDetector decoder = new QRCodeDetector();
-                points = new Mat();
-                String data = decoder.detectAndDecode(frame, points);
-            }
-        });
 
-        tradi.start();
-//
-//
-//
-//
-//        /***
-//         *  YOLO custom detector
-//         */
-//        Thread yolo = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                yoloQrDetector.getLayerNames();
-//                Mat frame_yolo = new Mat();
-//                Imgproc.cvtColor(frame, frame_yolo, Imgproc.COLOR_RGBA2RGB);
-//                Mat blob = Dnn.blobFromImage(frame_yolo, 1/255.0,
-//                        new org.opencv.core.Size(640, 640),
-//                        new Scalar(new double[]{0.0, 0.0, 0.0}), /*swapRB*/true, /*crop*/false, CV_32F);
-//                yoloQrDetector.setInput(blob);
-//
-//                //  -- determine  the output layer names that we need from YOLO
-//                // The forward() function in OpenCV’s Net class needs the ending layer till which it should run in the network.
-//
-//                List<String> layerNames = yoloQrDetector.getLayerNames();
-//                List<String> outputLayers = new ArrayList<String>();
-//                for (Integer i : yoloQrDetector.getUnconnectedOutLayers().toList()) {
-//                    outputLayers.add(layerNames.get(i - 1));
-//                }
-//
-//                List<Mat> outputs = new ArrayList<Mat>();
-//                yoloQrDetector.forward(outputs, outputLayers);
-//
-//                for(Mat output : outputs) {
-//                    //  loop over each of the detections. Each row is a candidate detection,
-//                    System.out.println("Output.rows(): " + output.rows() + ", Output.cols(): " + output.cols());
-//                    for (int i = 0; i < output.rows(); i++) {
-//                        Mat row = output.row(i);
-//                        List<Float> detect = new MatOfFloat(row).toList();
-//                        List<Float> score = detect.subList(5, output.cols());
-//                        int class_id = argmax(score); // index maximalnog elementa liste
-//                        float conf = score.get(class_id);
-//                        if (conf >= 0.5) {
-//                            int center_x = (int) (detect.get(0) * frame_yolo.cols());
-//                            int center_y = (int) (detect.get(1) * frame_yolo.rows());
-//                            int width = (int) (detect.get(2) * frame_yolo.cols());
-//                            int height = (int) (detect.get(3) * frame_yolo.rows());
-//
-//                            Imgproc.circle(frame, new Point(center_x,center_y), width, new Scalar(255, 0, 0), 5);
-//                            Imgproc.putText(frame, String.valueOf(conf), new Point(center_x,center_y), 2, 1, new Scalar(200, 255, 10));
-////                    int x = (center_x - width / 2);
-////                    int y = (center_y - height / 2);
-////                    Rect2d box = new Rect2d(x, y, width, height);
-////                    result.get("boxes").add(box);
-////                    result.get("confidences").add(conf);
-////                    result.get("class_ids").add(class_id);
-//                        }
-//                    }
-//                }
-//            }
-//        });
-//
-//        yolo.start();
-//
-////        /***
-////         * Boofcv
-////         */
-////
-////        // convert mat to boof cv
-////        Bitmap bmp = null;
-////        Mat rgb = new Mat();
-////        Imgproc.cvtColor(frame, rgb, Imgproc.COLOR_BGR2RGB);
-////        try {
-////            bmp = Bitmap.createBitmap(rgb.cols(), rgb.rows(), Bitmap.Config.ARGB_8888);
-////            Utils.matToBitmap(rgb, bmp);
-////        }
-////        catch (CvException e){
-////            Log.d("Exception",e.getMessage());
-////        }
-////
-////        // Easiest way to convert a Bitmap into a BoofCV type
-////        GrayU8 image = ConvertBitmap.bitmapToGray(bmp, (GrayU8)null, null);
-////
-////        ConfigQrCode config = new ConfigQrCode();
-//////		config.considerTransposed = false; // by default, it will consider incorrectly encoded markers. Faster if false
-////        QrCodeDetector<GrayU8> detector = FactoryFiducial.qrcode(config, GrayU8.class);
-////
-////        detector.process(image);
-////
-////        // Gets a list of all the qr codes it could successfully detect and decode
-////        List<QrCode> detections = detector.getDetections();
-////
-////        if(detections.size()>0)
-////        {
-////            Log.w(TAG, "Found QRCode with boofCV");
-////        }
+
+
+//        List<QrCode> listQr =  mQRCodeReader.Detect(frame, QRCodeReader.DetectionMethod.NORMAL);
 //
 //        try {
-////            wechat.join();
-////            yolo.join();
-//            tradi.join();
-//        } catch (InterruptedException e) {
+//            if (listQr.size() > 0) {
+//                // for each QRCode
+//                for (int i = 0; i < listQr.size(); i++) {
+//                    Log.w("sizescoucou", i + " " + listQr.get(i).rawContent + " " + listQr.get(i).matOfCorners.size());
+//                    x = (int) (listQr.get(i).matOfCorners.get(0, 0)[0]);
+//                    y = (int) (listQr.get(i).matOfCorners.get(0, 1)[0]);
+//                    Imgproc.circle(frame, new Point(x, y), 2, new Scalar(255, 0, 0), 10);
+//
+//                    x = (int) (listQr.get(i).matOfCorners.get(1, 0)[0]);
+//                    y = (int) (listQr.get(i).matOfCorners.get(1, 1)[0]);
+//                    Imgproc.circle(frame, new Point(x, y), 2, new Scalar(0, 255, 0), 10);
+//
+//                    x = (int) (listQr.get(i).matOfCorners.get(2, 0)[0]);
+//                    y = (int) (listQr.get(i).matOfCorners.get(2, 1)[0]);
+//                    Imgproc.circle(frame, new Point(x, y), 2, new Scalar(0, 0, 255), 10);
+//
+//                    x = (int) (listQr.get(i).matOfCorners.get(3, 0)[0]);
+//                    y = (int) (listQr.get(i).matOfCorners.get(3, 1)[0]);
+//                    Imgproc.circle(frame, new Point(x, y), 2, new Scalar(150, 0, 150), 10);
+//
+//
+//                    if (listQr.get(i).poseKnown) {
+//                        String angle = "" + listQr.get(i).getAngle(12.5);
+////                Log.w(TAG, "QRCOde trouve: " + angle + " " + listQr.get(0).getTranslationVector().get(1,0));
+//                        Imgproc.putText(frame, angle, new Point(x, y+20), 1, 2, new Scalar(0, 0, 255), 6);
+//                        Imgproc.putText(frame, angle, new Point(x, y+20), 1, 2, new Scalar(255, 255, 255), 2);
+//
+//                        Imgproc.putText(frame, "" + listQr.get(i).qrCodeTranslation.get(2, 0)[0], new Point(x, y+60), 1, 2, new Scalar(0, 0, 255), 6);
+//                        Imgproc.putText(frame, "" + listQr.get(i).qrCodeTranslation.get(2, 0)[0], new Point(x, y+60), 1, 2, new Scalar(255, 255, 255), 2);
+//                    } else {
+//                        Imgproc.putText(frame, "POSE UNKNONW", new Point((int) IMG_WIDTH / 4, (int) IMG_HEIGHT / 4), 1, 2, new Scalar(0, 0, 255), 6);
+//                        Imgproc.putText(frame, "POSE UNKNONW", new Point((int) IMG_WIDTH / 4, (int) IMG_HEIGHT / 4), 1, 2, new Scalar(255, 255, 255), 2);
+//                    } //end if pose known
+//                } //next qrcode
+//            } //end if list of QRCode empty
+//
+//        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
-//        /***************Display*******************/
-//        if (qrCodesContent.size()>0)
-//        {
-//            Log.w(TAG, "QRCode detected :" + qrCodesContent.get(0));
-//            Log.w(TAG, "QRCode position :" + qrCodesCorner.get(0).get(0,0)[0]);
-//
-//            x = (int) (qrCodesCorner.get(0).get(0,0)[0]);
-//            y = (int) (qrCodesCorner.get(0).get(0,1)[0]);
-//            Imgproc.circle(frame, new Point(x, y), 2, new Scalar(255, 0, 0), 10);
-//
-//            x = (int) (qrCodesCorner.get(0).get(1,0)[0]);
-//            y = (int) (qrCodesCorner.get(0).get(1,1)[0]);
-//            Imgproc.circle(frame, new Point(x, y), 2, new Scalar(0, 255, 0), 10);
-//
-//            x = (int) (qrCodesCorner.get(0).get(2,0)[0]);
-//            y = (int) (qrCodesCorner.get(0).get(2,1)[0]);
-//            Imgproc.circle(frame, new Point(x, y), 2, new Scalar(0, 0, 255), 10);
-//
-//            x = (int) (qrCodesCorner.get(0).get(3,0)[0]);
-//            y = (int) (qrCodesCorner.get(0).get(3,1)[0]);
-//            Imgproc.circle(frame, new Point(x, y), 2, new Scalar(150, 0, 150), 10);
-//
-//        }
-//
-//        if (!points.empty()) {
-//
-//            for (int i = 0; i < points.cols(); i++) {
-//                Point pt1 = new Point(points.get(0, i));
-//                Point pt2 = new Point(points.get(0, (i + 1) % 4));
-//                Imgproc.line(frame, pt1, pt2, new Scalar(150, 250, 0), 3);
-//            }
-//
-//        }
-//
-////        return frame;
-//
-//        Log.w("coucou", ""+supResMat.size());
-//        Imgproc.cvtColor(supResMat, supResMat, Imgproc.COLOR_YCrCb2BGR);
-//        Imgcodecs.imwrite("/storage/emulated/0/superres.jpg", supResMat);
-
-//        Imgproc.rectangle(frame , new Point((int) IMG_WIDTH/4,(int) IMG_HEIGHT/4),
-//                new Point((int) (3*IMG_WIDTH/4),(int) (3*IMG_HEIGHT/4)), new Scalar(250, 0, 0), 2 );
-
-//        List<QrCode> listQr =  mQRCodeReader.QRCodeDetectAndDecode(frame);
 
 
 
+        //crop image around face
+        Rect resizeROI= new Rect(0, 0,512, 384 );
+        Mat resized  = frame.submat(resizeROI);
+
+        Imgproc.resize(resized, resized , new Size(1024,768));
 
 
-//        List<QrCode> listQr =  mQRCodeReader.DetectAndDecode(frame);
-//        List<QrCode> listQr =  mQRCodeReader.Detect(frame, QRCodeReader.DetectionMethod.FAST);
-        List<QrCode> listQr =  mQRCodeReader.Detect(frame, QRCodeReader.DetectionMethod.HIGH_PRECISION);
-
-        try {
-            if (listQr.size() > 0) {
-                // for each QRCode
-                for (int i = 0; i < listQr.size(); i++) {
-                    Log.w("sizescoucou", i + " " + listQr.get(i).rawContent + " " + listQr.get(i).matOfCorners.size());
-                    x = (int) (listQr.get(i).matOfCorners.get(0, 0)[0]);
-                    y = (int) (listQr.get(i).matOfCorners.get(0, 1)[0]);
-                    Imgproc.circle(frame, new Point(x, y), 2, new Scalar(255, 0, 0), 10);
-
-                    x = (int) (listQr.get(i).matOfCorners.get(1, 0)[0]);
-                    y = (int) (listQr.get(i).matOfCorners.get(1, 1)[0]);
-                    Imgproc.circle(frame, new Point(x, y), 2, new Scalar(0, 255, 0), 10);
-
-                    x = (int) (listQr.get(i).matOfCorners.get(2, 0)[0]);
-                    y = (int) (listQr.get(i).matOfCorners.get(2, 1)[0]);
-                    Imgproc.circle(frame, new Point(x, y), 2, new Scalar(0, 0, 255), 10);
-
-                    x = (int) (listQr.get(i).matOfCorners.get(3, 0)[0]);
-                    y = (int) (listQr.get(i).matOfCorners.get(3, 1)[0]);
-                    Imgproc.circle(frame, new Point(x, y), 2, new Scalar(150, 0, 150), 10);
-
-
-                    if (listQr.get(i).poseKnown) {
-                        String angle = "" + listQr.get(i).getAngle(12.5);
-//                Log.w(TAG, "QRCOde trouve: " + angle + " " + listQr.get(0).getTranslationVector().get(1,0));
-                        Imgproc.putText(frame, angle, new Point(x, y+20), 1, 2, new Scalar(0, 0, 255), 6);
-                        Imgproc.putText(frame, angle, new Point(x, y+20), 1, 2, new Scalar(255, 255, 255), 2);
-
-                        Imgproc.putText(frame, "" + listQr.get(i).qrCodeTranslation.get(2, 0)[0], new Point(x, y+60), 1, 2, new Scalar(0, 0, 255), 6);
-                        Imgproc.putText(frame, "" + listQr.get(i).qrCodeTranslation.get(2, 0)[0], new Point(x, y+60), 1, 2, new Scalar(255, 255, 255), 2);
-                    } else {
-                        Imgproc.putText(frame, "POSE UNKNONW", new Point((int) IMG_WIDTH / 4, (int) IMG_HEIGHT / 4), 1, 2, new Scalar(0, 0, 255), 6);
-                        Imgproc.putText(frame, "POSE UNKNONW", new Point((int) IMG_WIDTH / 4, (int) IMG_HEIGHT / 4), 1, 2, new Scalar(255, 255, 255), 2);
-                    } //end if pose known
-                } //next qrcode
-            } //end if list of QRCode empty
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-//        Mat resized = new Mat();
-//        Imgproc.resize(frame, resized , new Size(1024,768));
-//        Rect displayROI= new Rect(
-//                0,
-//                0,
-//                resized.cols(),
-//                resized.rows() );
-//        Mat roiInDisplayMat = frame.submat(displayROI);
-//        resized.copyTo(roiInDisplayMat);
+        Rect displayROI= new Rect(
+                0,
+                0,
+                resized.cols(),
+                resized.rows() );
+        Mat roiInDisplayMat = frame.submat(displayROI);
+        resized.copyTo(roiInDisplayMat);
 
 
 
