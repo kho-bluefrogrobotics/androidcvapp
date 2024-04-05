80cced6ebfd8bd0870c4e61382d25abb527d679c
kho <kho@bluefrogrobotics.com>
2024-04-05 10:47:19 +0200

    better debug

--- app/src/main/java/com/bfr/opencvapp/grafcet/SpeedLinearGrafcet.java
+++ app/src/main/java/com/bfr/opencvapp/grafcet/SpeedLinearGrafcet.java
@@ -260,74 +260,105 @@ public class SpeedLinearGrafcet extends bfr_Grafcet {
 
                         if(obstacleL || obstacleR || obstacleM)
 //                            ||     bboxTooBig)
-                        {
+                        {   Log.i(name, "OBSTACLE L/M/R= " +
+                                BuddySDK.Sensors.TofSensors().FrontLeft().getDistance() + ","
+                                + BuddySDK.Sensors.TofSensors().FrontMiddle().getDistance() + ","
+                                + BuddySDK.Sensors.TofSensors().FrontRight().getDistance() + "\n step -> 250");
                             linearSpeed = 0.0f;
-                          step_num = 25;
+                            step_num = 250;
                         }
                         else{ //box OK and no pbstacle
-//                            if (personTracker.torsoHeight<=200 && personTracker.torsoHeight>150)
-//                                linearSpeed = 0.15f;
-//                            else if (personTracker.torsoHeight<=150 && personTracker.torsoHeight>130)
-//                                linearSpeed = 0.3f;
-//                            else if (personTracker.torsoHeight<=130 && personTracker.torsoHeight>120)
-//                                linearSpeed = 0.4f;
-//                            else if (personTracker.torsoHeight<=120 )
-//                                linearSpeed = 0.56f;
-//                            else
-//                                linearSpeed = 0.0f;
-
-
-                            if (personTracker.torsoHeight> 0.85*initialTorsoHeight) {
-                                Log.i("coucou", "Torso>Initial: " +personTracker.torsoHeight  + "/"+ initialTorsoHeight);
-                                linearSpeed = 0.0f;
-                            }
-                            else if (personTracker.torsoHeight<=0.85*initialTorsoHeight && personTracker.torsoHeight>0.75*initialTorsoHeight) {
-                                Log.i("coucou", "Torso>0.85*Initial: " +personTracker.torsoHeight  + "/"+ initialTorsoHeight);
+                            if (personTracker.torsoHeight<=350 && personTracker.torsoHeight>300)
+                            {
+                                Log.i(name, "Torso height = " + personTracker.torsoHeight + " -> step = 100");
                                 linearSpeed = 0.15f;
+                                step_num = 100;
                             }
-                            else if (personTracker.torsoHeight<=0.75*initialTorsoHeight && personTracker.torsoHeight>0.6*initialTorsoHeight) {
-                                Log.i("coucou", "Torso>0.75*Initial: " +personTracker.torsoHeight  + "/"+ initialTorsoHeight);
+                            else if (personTracker.torsoHeight<=300 && personTracker.torsoHeight>250) {
+                                Log.i(name, "Torso height = " + personTracker.torsoHeight + " -> step = 110");
                                 linearSpeed = 0.3f;
+                                step_num = 110;
                             }
-                            else if (personTracker.torsoHeight<=0.6*initialTorsoHeight && personTracker.torsoHeight>0.4*initialTorsoHeight) {
-                                Log.i("coucou", "Torso>0.6*Initial: " +personTracker.torsoHeight  + "/"+ initialTorsoHeight);
-                                linearSpeed = 0.45f;
+                            else if (personTracker.torsoHeight<=250 && personTracker.torsoHeight>160)
+                            {
+                                Log.i(name, "Torso height = " + personTracker.torsoHeight + " -> step = 120");
+                                linearSpeed = 0.4f;
+                                step_num = 120;
                             }
-                            else if (personTracker.torsoHeight<=0.4*initialTorsoHeight ) {
-                                Log.i("coucou", "Torso<0.6*Initial: " +personTracker.torsoHeight  + "/"+ initialTorsoHeight);
+                            else if (personTracker.torsoHeight<=160 )
+                            {
+                                Log.i(name, "Torso height = " + personTracker.torsoHeight + " -> step = 130");
                                 linearSpeed = 0.56f;
+                                step_num = 130;
                             }
-                            else {
-                                Log.i("coucou", "Torso - Initial ELSE: " +personTracker.torsoHeight  + "/"+ initialTorsoHeight);
+                            else
+                            {
+                                Log.i(name, "Torso height = " + personTracker.torsoHeight + " -> step = 140");
                                 linearSpeed = 0.0f;
+                                step_num = 140;
                             }
 
-                        }
 
+
+//                            if (personTracker.torsoHeight> 0.85*initialTorsoHeight) {
+//                                Log.i("coucou", "Torso>Initial: " +personTracker.torsoHeight  + "/"+ initialTorsoHeight);
+//                                linearSpeed = 0.0f;
+//                            }
+//                            else if (personTracker.torsoHeight<=0.85*initialTorsoHeight && personTracker.torsoHeight>0.75*initialTorsoHeight) {
+//                                Log.i("coucou", "Torso>0.85*Initial: " +personTracker.torsoHeight  + "/"+ initialTorsoHeight);
+//                                linearSpeed = 0.15f;
+//                            }
+//                            else if (personTracker.torsoHeight<=0.75*initialTorsoHeight && personTracker.torsoHeight>0.6*initialTorsoHeight) {
+//                                Log.i("coucou", "Torso>0.75*Initial: " +personTracker.torsoHeight  + "/"+ initialTorsoHeight);
+//                                linearSpeed = 0.3f;
+//                            }
+//                            else if (personTracker.torsoHeight<=0.6*initialTorsoHeight && personTracker.torsoHeight>0.4*initialTorsoHeight) {
+//                                Log.i("coucou", "Torso>0.6*Initial: " +personTracker.torsoHeight  + "/"+ initialTorsoHeight);
+//                                linearSpeed = 0.45f;
+//                            }
+//                            else if (personTracker.torsoHeight<=0.4*initialTorsoHeight ) {
+//                                Log.i("coucou", "Torso<0.6*Initial: " +personTracker.torsoHeight  + "/"+ initialTorsoHeight);
+//                                linearSpeed = 0.56f;
+//                            }
+//                            else {
+//                                Log.i("coucou", "Torso - Initial ELSE: " +personTracker.torsoHeight  + "/"+ initialTorsoHeight);
+//                                linearSpeed = 0.0f;
+//                            }
+
+                        }
 
                         break;
 
-
-
-                    case 20: // stabilize US detection
-                        Thread.sleep(100); // wait for next value
-                        if(obstacleL || obstacleR || obstacleM ||
-                                bboxTooBig)
-                        {
-                            linearSpeed = 0.0f;
-
-                        }
-                        step_num = 25;
+                    case 100 : //
+                        if(personTracker.torsoHeight>350 || personTracker.torsoHeight<300)
+                            step_num=15;
                         break;
 
+                    case 110://
+                        if (personTracker.torsoHeight>300 || personTracker.torsoHeight<250)
+                            step_num=15;
+                        break;
 
-                    case 25: // wait for OK to go
-                        if(!obstacleL && !obstacleR &&  !obstacleM &&
-                                !bboxTooBig)
-                        {
+                    case 120://
+                        if (personTracker.torsoHeight>250 || personTracker.torsoHeight<160)
+                            step_num=15;
+                        break;
+                    case 130://
+                        if (personTracker.torsoHeight>160 )
+                            step_num=15;
+                        break;
+                    case 140://
+
+                        if (personTracker.torsoHeight<350 )
+                            step_num=15;
+                        break;
+
+                    case 250://
+//                        linearSpeed = 0.0f;
+                        if(!obstacleL && !obstacleR && !obstacleM)
                             step_num = 15;
-                        }
                         break;
+
                     default:
                         // go to next step
                         step_num = 0;
