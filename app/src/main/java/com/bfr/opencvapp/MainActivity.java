6accf89da2eb6c02f5a9f6b9151a8ba60ee711cc
kho <kho@bluefrogrobotics.com>
2024-04-23 17:36:45 +0200

    WIP : display

--- app/src/main/java/com/bfr/opencvapp/MainActivity.java
+++ app/src/main/java/com/bfr/opencvapp/MainActivity.java
@@ -397,9 +397,12 @@ public class MainActivity extends BuddyActivity implements CameraBridgeViewBase.
                             bottom-top-10
                     );
                     Log.i(TAG, "To crop "+left + " " + top + " " + (right-left) + " " + (bottom-top) );
-                    Mat croppedTargetMat = frame.submat(toCrop);
+                    Mat croppedTargetMat = frame.clone().submat(toCrop);
 
                     Mat toSave = resizeWithPadding(croppedTargetMat, 256, 256);
+//                    Mat toSave = new Mat();
+//                    Imgproc.resize(croppedTargetMat, toSave, new Size( 256,256));
+
                     Imgproc.cvtColor(croppedTargetMat, croppedTargetMat, Imgproc.COLOR_BGR2RGB);
                     Imgproc.cvtColor(toSave, toSave, Imgproc.COLOR_BGR2RGB);
 
@@ -421,34 +424,35 @@ public class MainActivity extends BuddyActivity implements CameraBridgeViewBase.
 
                     float[][][][] result = blazePose.recognizeImage(bitmapImage);
 
-//                    // Blaze pose returns the x, y coords as values [0:255] independent from the input resolution
-//                    int noseX = (int) (result[0][0*5] * (right-left)/255);
-//                    int noseY = (int) (result[0][0*5+1] * (bottom-top)/255);
-//                    int leftEyeX = (int) (result[0][2*5] * (right-left)/255);
-//                    int leftEyeY = (int) (result[0][2*5+1] * (bottom-top)/255);
-//                    int rightEyeX = (int) (result[0][5*5] * (right-left)/255);
-//                    int rightEyeY = (int) (result[0][5*5+1] * (bottom-top)/255);
-//                    int leftShoulderX = (int) (result[0][11*5] * (right-left)/255);
-//                    int leftShoulderY = (int) (result[0][11*5+1] * (bottom-top)/255);
-//                    int rightShoulderX = (int) (result[0][12*5] * (right-left)/255);
-//                    int rightShoulderY = (int) (result[0][12*5+1] * (bottom-top)/255);
+
+//                    // Movenet returns the x, y coords as values [0:1] independent from the input resolution
+                    int noseX = (int) (result[0][0][0][1] * (right-left));
+                    int noseY = (int) (result[0][0][0][0] * (bottom-top));
+                    int leftEyeX = (int) (result[0][0][1][1]  * (right-left));
+                    int leftEyeY = (int) (result[0][0][1][0]  * (bottom-top));
+                    int rightEyeX = (int) (result[0][0][2][1]  * (right-left));
+                    int rightEyeY = (int) (result[0][0][2][0]  * (bottom-top));
+                    int leftShoulderX = (int) (result[0][0][6][1] * (right-left));
+                    int leftShoulderY = (int) (result[0][0][6][0] * (bottom-top));
+                    int rightShoulderX = (int) (result[0][0][7][1]  * (right-left));
+                    int rightShoulderY = (int) (result[0][0][7][0]  * (bottom-top));
 //                    int leftHipX = (int) (result[0][23*5] * (right-left)/255);
 //                    int leftHipY = (int) (result[0][23*5+1] * (bottom-top)/255);
 //                    int rightHipX = (int) (result[0][24*5] * (right-left)/255);
 //                    int rightHipY = (int) (result[0][24*5+1] * (bottom-top)/255);
-//                    //Log.w(TAG, "Coords : "+ shoulderX + " " +  shoulderY );
+                    //Log.w(TAG, "Coords : "+ shoulderX + " " +  shoulderY );
+
+                    Imgproc.circle(frame, new Point(
+                                    left + noseX, top + noseY), 5, new Scalar(0,255,0), 10);
+                    Imgproc.circle(frame, new Point(
+                            left + leftEyeX, top + leftEyeY), 5, new Scalar(0,255,0), 10);
+                    Imgproc.circle(frame, new Point(
+                            left + rightEyeX, top + rightEyeY), 5, new Scalar(0,255,0), 10);
 //
-//                    Imgproc.circle(frame, new Point(
-//                                    left + noseX, top + noseY), 5, new Scalar(0,255,0), 10);
-//                    Imgproc.circle(frame, new Point(
-//                            left + leftEyeX, top + leftEyeY), 5, new Scalar(0,255,0), 10);
-//                    Imgproc.circle(frame, new Point(
-//                            left + rightEyeX, top + rightEyeY), 5, new Scalar(0,255,0), 10);
-//
-//                    Imgproc.circle(frame, new Point(
-//                            left + leftShoulderX, top + leftShoulderY), 5, new Scalar(0,255,0), 10);
-//                    Imgproc.circle(frame, new Point(
-//                            left + rightShoulderX, top + rightShoulderY), 5, new Scalar(0,255,0), 10);
+                    Imgproc.circle(frame, new Point(
+                            left + leftShoulderX, top + leftShoulderY), 5, new Scalar(0,255,0), 10);
+                    Imgproc.circle(frame, new Point(
+                            left + rightShoulderX, top + rightShoulderY), 5, new Scalar(0,255,0), 10);
 //
 //                    Imgproc.circle(frame, new Point(
 //                            left + leftHipX, top + leftHipY), 5, new Scalar(0,255,0), 10);
